33d63f9510c428923f2bf5254d08df376237e909
Lukas Jakob <lu.jakob@googlemail.com>
2016-10-05 13:50:52 +0200

    Add config file and rename offset and limit parameters.

--- app/services/client.service.ts
+++ app/services/client.service.ts
@@ -1,10 +1,11 @@
 import { Injectable }     from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable }     from 'rxjs/Observable';
+import { REST_ENDPOINT } from '../config/config';
 import 'rxjs/add/operator/map';
 import 'rxjs/add/operator/catch';
 
-export const BASE_URL = "http://localhost:3333/clients";
+export const BASE_URL = REST_ENDPOINT;
 // const BASE_URL = "data/statementsData.json";
 
 export type IAPIClients = {
